33dec9e30a32065b0bf1129e32bb0c209c150231
trumpetcoder <kevindjames55@gmail.com>
2018-02-24 16:05:56 -0700

    components

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,12 +4,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EditReviewsComponent } from './components/edit-reviews/edit-reviews.component';
+import { ReviewDetailsComponent } from './components/review-details/review-details.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DashboardComponent,
+    EditReviewsComponent,
+    ReviewDetailsComponent,
+    LoginComponent,
+    RegisterComponent,
+    
+    
   ],
   imports: [
     BrowserModule
---
+++ front-end/src/app/components/edit-reviews/edit-reviews.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-reviews',
+  templateUrl: './edit-reviews.component.html',
+  styleUrls: ['./edit-reviews.component.css']
+})
+export class EditReviewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
