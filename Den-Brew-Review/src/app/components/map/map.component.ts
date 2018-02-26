20c8899e82723a0240f19d06aeb76b3fe48746b3
trumpetcoder <kevindjames55@gmail.com>
2018-02-26 15:51:34 -0700

    added marker click functionality, need to work on

--- Den-Brew-Review/src/app/components/map/map.component.ts
+++ Den-Brew-Review/src/app/components/map/map.component.ts
@@ -6,8 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-  lat: number = 39.7392;
-  lng: number = -104.9903;
+  latitude: number = 39.7392; // Denver Lat + Lng
+  longitude: number = -104.9903;
+
+  onChoseLocation(event) {
+    console.log(event); // Checking to see if click is registering
+    this.latitude = event.coords.lat;
+    this.longitude = event.coords.lng;
+  }
 
   constructor() { }
 
