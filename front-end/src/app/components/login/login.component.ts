bcf5680b6c21b935eddc2bb77fb1e93a9d9c73f2
trumpetcoder <kevindjames55@gmail.com>
2018-02-25 17:09:32 -0700

    found mistake in login form fixed and working!!!!!!!

--- front-end/src/app/components/login/login.component.ts
+++ front-end/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService} from '../../services/auth.service'; //bringing in auth service
+import { FlashMessagesService } from 'angular2-flash-messages'; //using flash messaging
+import { Router } from '@angular/router'; //bringing in router
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,31 @@ import { AuthService} from '../../services/auth.service'; //bringing in auth ser
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flashMessage: FlashMessagesService
+  ) { }
 
   ngOnInit() {
+  }
+  // Logic buildout for the login form with error handling and flashmessaging
+  onSubmit() {
+    this.authService.login(this.email, this.password)
+    .then(res => {
+      this.flashMessage.show('You are now logged in', {
+        cssClass: 'alert-success', timeout: 4000
+      });
+      this.router.navigate(['/']);
+    })
+    .catch(err => {
+      this.flashMessage.show(err.message, {
+        cssClass: 'alert-danger', timeout: 4000
+      });
+    });
   }
 
 }
